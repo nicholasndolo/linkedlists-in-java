6204af3f0f3afa4ea3e617867fa06b251a9161a1
Nicholas <nickndolo95@gmail.com>
2024-09-24 13:30:40 +0300

    add contains method

--- src/com/LinkedLists/LinkedList.java
+++ src/com/LinkedLists/LinkedList.java
@@ -49,6 +49,10 @@ public class LinkedList {
         return -1;
     }
 
+    public boolean contains(int item){
+        return indexOf(item) != -1;
+    }
+
 
     private boolean isEmpty(){
         return first == null;
