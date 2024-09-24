f6de3c19d481fca4beb1754e790816bb8738bcdc
Nicholas <nickndolo95@gmail.com>
2024-09-24 08:37:27 +0300

    explore LinkedList class and its useful methods

---
+++ src/com/LinkedLists/Main.java
@@ -0,0 +1,23 @@
+package com.LinkedLists;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(5);
+        list.removeLast();
+        list.removeFirst();
+
+        var array = list.toArray();
+
+        System.out.println(list.contains(40));
+        System.out.println(list.indexOf(20));
+        System.out.println(list.size());
+        System.out.println(Arrays.toString(array));
+    }
+}
