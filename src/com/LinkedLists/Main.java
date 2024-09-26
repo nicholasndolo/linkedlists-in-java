83f4381710e6f8ed0d047e132ca9c70a0391c92c
Nicholas <nickndolo95@gmail.com>
2024-09-26 14:50:22 +0300

    add reverse method

--- src/com/LinkedLists/Main.java
+++ src/com/LinkedLists/Main.java
@@ -20,9 +20,11 @@ public class Main {
 //        System.out.println(Arrays.toString(array));
 
         var list = new LinkedList();
-       list.addLast(10);
-       list.addLast(20);
-       list.addLast(30);
+//       list.addLast(10);
+//       list.addLast(20);
+//       list.addLast(30);
+
+       list.reverse();
 
        var array = list.toArray();
 
